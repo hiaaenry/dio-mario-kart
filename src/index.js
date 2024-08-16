f3afac2b2852c98d715a39526bd214e0d30ea1cb
Rayane Sales <rayane.sales@ne.team>
2024-08-16 09:27:17 -0300

    feat: player selection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("Play")
+import { createInterface } from 'readline';
+import { mario, peach, yoshi, bowser, luigi, donkeyKong } from './players.js';
+
+function askQuestion(rl, question) {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer)
+        })
+    })
+}
+
+async function selectPlayer() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    const players = [mario, peach, yoshi, bowser, luigi, donkeyKong]
+
+    let selectedPlayer;
+
+    while (!selectedPlayer) {
+        console.table(players)
+        const index = await askQuestion(rl, 'Choose an index: ');
+        const selectedPlayerIndex = parseInt(index, 10);
+
+        if (selectedPlayerIndex >= 0 && selectedPlayerIndex < players.length) {
+            selectedPlayer = players[selectedPlayerIndex]
+            players.splice(selectedPlayerIndex, 1)
+        } else {
+            console.log('Invalid number. Please choose a number between 0 and 5.')
+        }
+    }
+
+    let selectedOpponent;
+
+    const randomOpponent = Math.floor(Math.random() * players.length)
+    selectedOpponent = players[randomOpponent]
+
+    rl.close();
+    return {
+        selectedPlayer,
+        selectedOpponent
+    }
+}
+
+(async function main() {
+    const { selectedPlayer, selectedOpponent } = await selectPlayer()
+    console.log(`Race between ${selectedPlayer.name} and ${selectedOpponent.name}`);
+})()
---
+++ src/players.js
@@ -0,0 +1,41 @@
+export const mario = {
+    name: "Mario",
+    speed: 4,
+    handling: 3,
+    power: 3,
+}
+
+export const peach = {
+    name: "Peach",
+    speed: 3,
+    handling: 4,
+    power: 2,
+}
+
+export const yoshi = {
+    name: "Yoshi",
+    speed: 2,
+    handling: 4,
+    power: 3,
+}
+
+export const bowser = {
+    name: "Bowser",
+    speed: 5,
+    handling: 2,
+    power: 5,
+}
+
+export const luigi = {
+    name: "Luigi",
+    speed: 3,
+    handling: 4,
+    power: 4,
+}
+
+export const donkeyKong = {
+    name: "Donkey Kong",
+    speed: 2,
+    handling: 2,
+    power: 5,
+}
